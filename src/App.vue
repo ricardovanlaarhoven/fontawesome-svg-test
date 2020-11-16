8d49c2c4c11f09807a3324464c9372030b8f2db8
ricardovanlaarhoven <ricardo1994@gmail.com>
2020-11-16 11:25:39 +0100

    add vuetify fontawesome svg test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      v-icon:
+      <v-icon>fas fa-user-secret</v-icon>
+      <v-icon>fa-user-secret</v-icon>
+      <v-icon>user-secret</v-icon>
+      <v-icon>cancel</v-icon>
+      font-awesome-icon:
+      <font-awesome-icon icon="user-secret"></font-awesome-icon>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
